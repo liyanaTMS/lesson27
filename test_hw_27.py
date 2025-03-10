f6e746282d15157d55d6b74d3127045ac66dd863
liana lesnikovich <liyana.lesnikovich.run4fun@gmail.com>
2025-03-11 02:03:52 +0300

    hw 27

--- test_hw_27.py
+++ test_hw_27.py
@@ -1,10 +1,12 @@
 import sqlite3
 import pytest
 
-def test_work_sql():
+def test_hw27_sqlite():
+    # Подключаемся к базе данных (создаст файл, если его нет)
     conn = sqlite3.connect("university.db")
-    cursor = conn.cursor()
+    cursor = conn.cursor()  # Создаем курсор - это специальный объект который делает запросы и получает их результаты
 
+    # Создаем таблицу
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -14,27 +16,34 @@ def test_work_sql():
         country TEXT DEFAULT 'Unknown'
     );
     """)
+    # Фиксируем изменения
     conn.commit()
 
-    cursor.execute("""
-    INSERT OR REPLACE INTO users (name, email, age, country) VALUES (?, ?, ?, ?)
-    """,("Mike", "mike@example.com", 26, "England"))
+    # Добавляем юзера
+    cursor.execute("INSERT INTO users (name, email, age, country) VALUES (?, ?, ?, ?)",
+                   ("test_user", "test_user@gmail.com", 41, "Belarus"))
 
+    # Фиксируем изменения
     conn.commit()
-    print("Успешно, возможно заменено")
 
-    cursor.execute("SELECT email FROM users WHERE email = 'mike@example.com'")
+    # Проверяем результат добавления пользователя
+    cursor.execute("SELECT * FROM users WHERE name = 'test_user'")
     rows = cursor.fetchall()
     for row in rows:
-        print(row)
+        print("Next user was added: ", row)
+    if len(rows) != 0:
+        print('Пользователь успешно добавлен')
 
-    cursor.execute("DELETE FROM users WHERE email = 'mike@example.com'")
+    # Удаляем добавленного пользователя
+    cursor.execute("DELETE FROM users WHERE name = 'test_user'")
+
+    # Фиксируем изменения
     conn.commit()
 
-    cursor.execute("SELECT email FROM users WHERE email = 'mike@example.com'")
+    # Проверяем результат удаления пользователя
+    cursor.execute("SELECT * FROM users WHERE name = 'test_user'")
     rows = cursor.fetchall()
     if len(rows) == 0:
-        print('Успешно удалено')
-
+        print('Пользователь успешно удален')
 
     conn.close()
